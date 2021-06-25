7945ebb02e94bcc6a17a3b3870c6477c0b2509c4
Gustav <uszyc@student.kit.edu>
2021-06-26 00:52:03 +0200

    design ModuleCreationComponent

--- src/components/creation/ModuleCreationComponent.tsx
+++ src/components/creation/ModuleCreationComponent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Button, Container, Grid, Paper, TextField} from "@material-ui/core";
+import {Container, Grid, IconButton, Paper, TextField} from "@material-ui/core";
+import {CameraAlt, Close, FeaturedVideo, FilterFrames, Forum, Help, Link, Message, Mic, Room, Publish, Videocam} from "@material-ui/icons";
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import "./ModuleCreationComponent.css";
 
@@ -11,39 +12,107 @@ export default function ModuleCreationComponent({props}) {
 	//render() {
 		return (
 			<div className={"creation-div"}>
-				<Button variant={"contained"} color={"primary"} onClick={props.onBackButtonClicked}>Back!</Button>
-				<Grid container className={styles.root} spacing={2} direction="row" justify="center">
-					<Grid item xs={12}>
-						<Paper >
+				<TextField id="module-name-input" label="Module Name" variant="outlined"/>
+				<Grid container className={styles.container} spacing={3}>
+					<Grid item xs={2}>
+						<Paper className={styles.paper}>
 							Tools
+							<Grid container>
+								<Grid item xs={6}>
+									<IconButton component="span" onClick={props.onBackButtonClicked}>
+        							  <Videocam fontSize="large"/>
+        							</IconButton>
+								</Grid>
+								<Grid item xs={6}>
+									<IconButton component="span" onClick={props.onBackButtonClicked}>
+        							  <Message fontSize="large"/>
+        							</IconButton>
+								</Grid>
+								<Grid item xs={6}>
+									<IconButton component="span" onClick={props.onBackButtonClicked}>
+        							  <Mic fontSize="large"/>
+        							</IconButton>
+								</Grid>
+								<Grid item xs={6}>
+									<IconButton component="span" onClick={props.onBackButtonClicked}>
+        							  <Room fontSize="large"/>
+        							</IconButton>
+								</Grid>
+								<Grid item xs={6}>
+									<IconButton component="span" onClick={props.onBackButtonClicked}>
+        							  <CameraAlt fontSize="large"/>
+        							</IconButton>
+								</Grid>
+								<Grid item xs={6}>
+									<IconButton component="span" onClick={props.onBackButtonClicked}>
+        							  <Link fontSize="large"/>
+        							</IconButton>
+								</Grid>
+								<Grid item xs={6}>
+									<IconButton component="span" onClick={props.onBackButtonClicked}>
+        							  <Forum fontSize="large"/>
+        							</IconButton>
+								</Grid>
+								<Grid item xs={6}>
+									<IconButton component="span" onClick={props.onBackButtonClicked}>
+        							  <Help fontSize="large"/>
+        							</IconButton>
+								</Grid>
+								<Grid item xs={6}>
+									<IconButton component="span" onClick={props.onBackButtonClicked}>
+        							  <FilterFrames fontSize="large"/>
+        							</IconButton>
+								</Grid>
+								<Grid item xs={6}>
+									<IconButton component="span" onClick={props.onBackButtonClicked}>
+        							  <FeaturedVideo fontSize="large"/>
+        							</IconButton>
+								</Grid>
+							</Grid>
 						</Paper>
 					</Grid>
-					<Grid item xs={12}>
-						<Container maxWidth="sm">
-							<TextField id="module-name-input" label="Module Name" variant="outlined" />
-							<Paper>
-								Module
-							</Paper>
-						</Container>
+					<Grid item xs={10}>
+						<Paper className={styles.paper}>
+							Module
+						</Paper>
 					</Grid>
 				</Grid>
-				<Button variant={"contained"} color={"primary"} onClick={() => { }}>Publish!</Button>
+				<Grid container>
+					<Grid item>
+						<IconButton component="span" onClick={props.onBackButtonClicked}>
+        				  <Close fontSize="large"/>
+						  Cancel
+        				</IconButton>
+					</Grid>
+					<Grid item>
+						<IconButton component="span" onClick={props.onBackButtonClicked}>
+        				  <Publish fontSize="large"/>
+						  Publish
+        				</IconButton>
+					</Grid>
+				</Grid>
 			</div>
 		)
 	//}
 }
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      height: 140,
-      width: 100,
-    },
-    control: {
-      padding: theme.spacing(2),
-    },
-  }),
+	createStyles({
+		container: {
+			//flexGrow: 1,
+			display: "flex",
+			direction: "row",
+			justify: "center",
+			alignItems: "center",
+		},
+		module: {
+			direction: "column",
+			justify: "center",
+			alignItems: "center",
+		},
+		paper: {
+			textAlign: 'center',
+			height: 310,
+		},
+	}),
 );
