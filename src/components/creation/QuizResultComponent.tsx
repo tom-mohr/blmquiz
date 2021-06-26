a3e75228427f1b29914962d8e7b68d34ae5bff4a
Gustav <uszyc@student.kit.edu>
2021-06-26 23:48:42 +0200

    add dialog to add image URL

--- src/components/creation/QuizResultComponent.tsx
+++ src/components/creation/QuizResultComponent.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { InputLabel, MenuItem, FormControl, Paper, Slider, TextField, Typography } from "@material-ui/core";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, Paper, Slider, TextField, Typography } from "@material-ui/core";
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { GetApp } from "@material-ui/icons";
 import { QuizResult } from "../../logic/quiz";
 import { getExtremesPerCategory } from "../../logic/quizAnswerMatching";
 
 export default function QuizResultComponent({ props, callback }) {
 
     const [result, setResult] = React.useState<QuizResult>(props.result);
+    const [dialogOpen, setDialogOpen] = React.useState(false);
 
     const styles = useStyles();
 
@@ -20,15 +22,60 @@ export default function QuizResultComponent({ props, callback }) {
       setResult({ ...result, title: event.target.value });
     };
 
+    const handleImageURL = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setResult({ ...result, imageUrl: event.target.value });
+    };
+
     const handleIdealScores = (index) => (event: any, newValue: number | number[]) => {
         const newIdealScores = result.idealScores;
         newIdealScores[index] = newValue as number;
         setResult({ ...result, idealScores: newIdealScores});
     };
 
+    const handleClickOpen = () => {
+        setDialogOpen(true);
+    };
+
+    const handleClose = () => {
+        setDialogOpen(false);
+    };
+
     return (
         <Paper className={styles.paper} elevation={6}>
             <TextField className={styles.title} variant="outlined" value={result.title} onChange={handleTitle()} />
+            {result.imageUrl === "" ? (
+                <IconButton component="span" onClick={handleClickOpen}>
+                    <GetApp/>
+                    Image
+                </IconButton>
+            ) :
+            <TextField className={styles.title} label="Image URL" value={result.imageUrl} onChange={handleImageURL} />
+            }
+            <Dialog open={dialogOpen} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Upload Image</DialogTitle>
+                <DialogContent>
+                    <IconButton component="span" onClick={() => {}}>
+                        <GetApp/>
+                        Upload
+                    </IconButton>
+                    <DialogContentText>
+                        or
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="Image URL"
+                        value={result.imageUrl}
+                        onChange={handleImageURL}
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
             <Typography variant="h6" gutterBottom>
                 Ideal score to achieve:
             </Typography>
--- src/components/creation/subcomponents/AnswerCardComponent.tsx
+++ src/components/creation/subcomponents/AnswerCardComponent.tsx
@@ -1,33 +1,80 @@
 import React from 'react';
-import { InputLabel, MenuItem, FormControl, Paper, Select, TextField, Typography } from "@material-ui/core";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, Paper, TextField, Typography } from "@material-ui/core";
+import { GetApp } from "@material-ui/icons";
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { QuizPossibleAnswer } from "../../../logic/quiz";
 
 export default function AnswerCardComponent({ props, extraProps, callback }) {
 
     const [answer, setAnswer] = React.useState<QuizPossibleAnswer>(props);
+    const [dialogOpen, setDialogOpen] = React.useState(false);
 
     const styles = useStyles();
 
-    const updateAnswer = React.useCallback((ans) => {callback(ans)}, [callback]);
+    const updateAnswer = React.useCallback((ans) => { callback(ans) }, [callback]);
 
     React.useEffect(() => {
         updateAnswer(answer);
     }, [answer])
 
-    const handleTitle = () => (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
         setAnswer({ ...answer, title: event.target.value });
+    };
+
+    const handleImageURL = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setAnswer({ ...answer, imageUrl: event.target.value });
     };
 
     const handleScoreDelta = (index) => (event: React.ChangeEvent<HTMLInputElement>) => {
         const newScoresDelta = answer.scoresDelta;
         newScoresDelta[index] = parseInt(event.target.value, 10);
-        setAnswer({ ...answer, scoresDelta: newScoresDelta});
+        setAnswer({ ...answer, scoresDelta: newScoresDelta });
+    };
+
+    const handleClickOpen = () => {
+        setDialogOpen(true);
+    };
+
+    const handleClose = () => {
+        setDialogOpen(false);
     };
 
     return (
         <Paper className={styles.paper}>
-            <TextField className={styles.title} variant="outlined" value={answer.title} onChange={handleTitle()} />
+            <TextField className={styles.title} label="Answer" variant="outlined" value={answer.title} onChange={handleTitle} />
+            {answer.imageUrl === "" ? (
+                <IconButton component="span" onClick={handleClickOpen}>
+                    <GetApp/>
+                    Image
+                </IconButton>
+            ) :
+            <TextField className={styles.title} label="Image URL" value={answer.imageUrl} onChange={handleImageURL} />
+            }
+            <Dialog open={dialogOpen} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Upload Image</DialogTitle>
+                <DialogContent>
+                    <IconButton component="span" onClick={() => {}}>
+                        <GetApp/>
+                        Upload
+                    </IconButton>
+                    <DialogContentText>
+                        or
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="Image URL"
+                        value={answer.imageUrl}
+                        onChange={handleImageURL}
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
             {extraProps.map((score, index) => (
                 <div>
                     <Typography variant="button" gutterBottom>
