8ebfeffbe2d8d8b4cdc0228d9a555eb88f1b0879
tom.mohr <tom-mohr@users.noreply.github.com>
2021-06-27 12:24:50 +0200

    translated quiz

--- src/logic/examples.ts
+++ src/logic/examples.ts
@@ -3,56 +3,76 @@ import {Module, Quiz, Tool, ToolType} from "./quiz";
 export const exampleModules: Module[] = [
     {
         id: 0,
-        title: "Welche Epoche passt am besten zu dir?",
+        title: "Which epoch fits you the best?",
         imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Huber_Carl_III.Wilhelm.jpeg/256px-Huber_Carl_III.Wilhelm.jpeg",
         tools: [
             {
+                id: 0,
                 type: ToolType.Quiz,
-                scoreNames: ["Mittelalter", "Barock", "Neuzeit"],
+                scoreNames: ["Middle Ages", "Baroque", "Modern Era"],
                 results: [
                     {
-                        title: "Mittelalter",
+                        title: "Middle Ages",
                         idealScores: [2, 1, -1],
+                        description: "You are perfectly suited for fighting against dragons.",
+                        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/1/11/Otto_I_Manuscriptum_Mediolanense_c_1200.jpg",
                     },
                     {
-                        title: "Barock",
+                        title: "Baroque",
                         idealScores: [1, 3, -1],
                     },
                     {
-                        title: "Neuzeit",
+                        title: "Modern Era",
                         idealScores: [-3, -1, 2],
                     },
                 ],
                 questions: [
                     {
-                        title: "Wie wichtig ist dir Hygiene?",
+                        title: "How important is hygiene to you?",
                         possibleAnswers: [
                             {
-                                title: "sehr wichtig",
+                                title: "Very important, I wash my hands all the time.",
                                 scoresDelta: [-2, -1, 1],
                                 imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/OCD_handwash.jpg/128px-OCD_handwash.jpg",
                             },
                             {
-                                title: "so mittel",
+                                title: "I shower once a week.",
                                 scoresDelta: [-1, -1, 0],
                             },
                             {
-                                title: "völlig unwichtig",
+                                title: "Shower? What is that?",
                                 scoresDelta: [2, 1, -1],
                                 imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Paul_F%C3%BCrst%2C_Der_Doctor_Schnabel_von_Rom_%28coloured_version%29.png/128px-Paul_F%C3%BCrst%2C_Der_Doctor_Schnabel_von_Rom_%28coloured_version%29.png",
                             }
                         ],
                     },
                     {
-                        title: "Legst du Wert auf politische Mitbestimmung?",
+                        title: "Do you value political participation?",
                         possibleAnswers: [
                             {
-                                title: "ja",
+                                title: "Democracy rules!",
                                 scoresDelta: [-1, -2, 1],
                             },
                             {
-                                title: "nein",
+                                title: "Politics is for nerds.",
                                 scoresDelta: [0, 2, 0],
+                            },
+                        ],
+                    },
+                    {
+                        title: "Are you religious?",
+                        possibleAnswers: [
+                            {
+                                title: "I go to church every sunday.",
+                                scoresDelta: [2, 1, 0],
+                            },
+                            {
+                                title: "I'm not sure, but Christmas is nice!",
+                                scoresDelta: [-1, 1, 1],
+                            },
+                            {
+                                title: "Pie in the sky? Doesn't make sense to me.",
+                                scoresDelta: [-3, 0, 1],
                             },
                         ],
                     }
@@ -62,24 +82,24 @@ export const exampleModules: Module[] = [
     },
     {
         id: 0,
-        title: "Mittelalter: Welchem Stand gehörst du an?",
+        title: "Middle Ages: Which estate of the realm do you belong to?",
         tools: [],
     },
     {
         id: 0,
-        title: "Welcher Herrscher-Typ bist du?",
+        title: "What kind of ruler are you?",
         imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Statue-Augustus.jpg/256px-Statue-Augustus.jpg",
         tools: [],
     },
     {
         id: 0,
-        title: "Finde deine historische Seelenverwandtschaft",
+        title: "Find your historical role model",
         imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Carl_Friedrich_Gauss.jpg/256px-Carl_Friedrich_Gauss.jpg",
         tools: [],
     },
     {
         id: 0,
-        title: "Teste deine Steinzeit-Instinkte",
+        title: "Test your Stone Age instincts",
         tools: [],
     },
 ];
